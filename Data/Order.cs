3a7baaca9fa4a5816b8912848c8f2c595637334c
Angel Hernandez <angel75@ksu.edu>
2020-02-26 16:08:13 -0600

    tests begin

---
+++ Data/Order.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CowboyCafe.Data
+{
+    public class Order
+    {
+        public double Subtotal => 0;
+        public IEnumerable<IOrderItem> Items => throw new NotImplementedException();
+
+        public void Add(IOrderItem item)
+        {
+
+        }
+
+        public void Remove(IOrderItem item)
+        {
+
+        }
+
+    }
+}
---
+++ DataTests/UnitTests/OrderTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using CowboyCafe.Data;
+
+namespace CowboyCafe.DataTests
+{
+    public class OrderTests
+    {
+        public class MockOrderItem : IOrderItem
+        {
+            public double Price { get; set; }
+            public List<string> SpecialInstructions { get; set; }
+        }
+
+        [Fact]
+        public void ShouldBeAbleToAddItems()
+        {
+            var order = new Order();
+            var item = new MockOrderItem();
+            order.Add(item);
+            Assert.Contains(item, order.Items);
+        }
+
+        [Fact]
+        public void ShouldBeAbleToRemoveItems()
+        {
+            var order = new Order();
+            var item = new MockOrderItem();
+            order.Add(item);
+            order.Remove(item);
+            Assert.DoesNotContain(item, order.Items);
+        }
+
+        [Fact]
+        public void ShouldBeAbleToGetEnumerationOfItems()
+        {
+            var order = new Order();
+            var i0 = new MockOrderItem();
+            var i1 = new MockOrderItem();
+            var i2 = new MockOrderItem();
+
+            order.Add(i0);
+            order.Add(i1);
+            order.Add(i2);
+
+            Assert.Collection(order.Items,
+                    i0 => { },
+                    i1 => { },
+                    i2 => { });
+        }
+
+        // Subtotal should be right
+    }
+}
