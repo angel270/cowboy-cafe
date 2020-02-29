4b8ae99f3f878035c3745ec0673c8267fa90098f
Angel Hernandez <angel75@ksu.edu>
2020-02-28 21:52:51 -0600

    im so tired - Controls Added

---
+++ PointOfSale/MenuItemSelectionControl.xaml.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CowboyCafe.Data;
+
+namespace PointOfSale
+{
+    /// <summary>
+    /// Interaction logic for MenuItemSelectionControl.xaml
+    /// </summary>
+    public partial class MenuItemSelectionControl : UserControl
+    {
+        public MenuItemSelectionControl()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// Handles texas triple button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AddTexasTriple_Click(object sender, RoutedEventArgs e)
+        {
+            ItemsOrdered.Items.Add(new TexasTripleBurger());
+        }
+        /// <summary>
+        /// Handles dakota double button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AddDakotaDouble_Click(object sender, RoutedEventArgs e)
+        {
+            ItemsOrdered.Items.Add(new DakotaDoubleBurger());
+        }
+        /// <summary>
+        /// Handles trail burger button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AddTrailBurger_Click(object sender, RoutedEventArgs e)
+        {
+            ItemsOrdered.Items.Add(new TrailBurger());
+        }
+        /// <summary>
+        /// Handles pecos pulled pork button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AddPecosPulledPork_Click(object sender, RoutedEventArgs e)
+        {
+            ItemsOrdered.Items.Add(new PecosPulledPork());
+        }
+        /// <summary>
+        /// Handles rustlers ribs button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AddRustlersRibs_Click(object sender, RoutedEventArgs e)
+        {
+            ItemsOrdered.Items.Add(new RustlersRibs());
+        }
+        /// <summary>
+        /// Handles cowpoke chili button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AddCowpokeChili_Click(object sender, RoutedEventArgs e)
+        {
+            ItemsOrdered.Items.Add(new CowpokeChili());
+        }
+        /// <summary>
+        /// Handles angry chicken button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AddAngryChicken_Click(object sender, RoutedEventArgs e)
+        {
+            ItemsOrdered.Items.Add(new AngryChicken());
+        }
+        /// <summary>
+        /// Handles chili cheese fries button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AddChiliCheeseFries_Click(object sender, RoutedEventArgs e)
+        {
+            ItemsOrdered.Items.Add(new ChiliCheeseFries());
+        }
+        /// <summary>
+        /// Handles pan de campo button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AddPanDeCampo_Click(object sender, RoutedEventArgs e)
+        {
+            ItemsOrdered.Items.Add(new PanDeCampo());
+        }
+        /// <summary>
+        /// Handles corn dodgers button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AddCornDodgers_Click(object sender, RoutedEventArgs e)
+        {
+            ItemsOrdered.Items.Add(new CornDodgers());
+        }
+        /// <summary>
+        /// Handles baked beans button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AddBakedBeans_Click(object sender, RoutedEventArgs e)
+        {
+            ItemsOrdered.Items.Add(new BakedBeans());
+        }
+        /// <summary>
+        /// Handles jerked soda button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AddJerkedSoda_Click(object sender, RoutedEventArgs e)
+        {
+            ItemsOrdered.Items.Add(new JerkedSoda());
+        }
+
+        private void AddTexasTea_Click(object sender, RoutedEventArgs e)
+        {
+            ItemsOrdered.Items.Add(new TexasTea());
+        }
+
+        private void AddCowboyCoffee_Click(object sender, RoutedEventArgs e)
+        {
+            ItemsOrdered.Items.Add(new CowboyCoffee());
+        }
+
+        private void AddWater_Click(object sender, RoutedEventArgs e)
+        {
+            ItemsOrdered.Items.Add(new Water());
+        }
+    }
+}
--- PointOfSale/OrderSummaryControl.xaml.cs
+++ PointOfSale/OrderSummaryControl.xaml.cs
@@ -10,16 +10,15 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using CowboyCafe.Data;
 
 namespace PointOfSale
 {
     /// <summary>
-    /// Interaction logic for OrderControl.xaml
+    /// Interaction logic for OrderSummaryControl.xaml
     /// </summary>
-    public partial class OrderControl : UserControl
+    public partial class OrderSummaryControl : UserControl
     {
-        public OrderControl()
+        public OrderSummaryControl()
         {
             InitializeComponent();
         }
