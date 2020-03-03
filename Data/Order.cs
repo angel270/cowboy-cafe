89e4a2212562846c4d4108e9e68ca75b618083a9
Angel Hernandez <angel75@ksu.edu>
2020-03-02 20:25:23 -0600

    Documented PS 2

--- Data/Order.cs
+++ Data/Order.cs
@@ -6,9 +6,21 @@ namespace CowboyCafe.Data
 {
     public class Order : INotifyPropertyChanged
     {
+        /// <summary>
+        /// Keeps track of the order number next available
+        /// </summary>
         private static uint lastOrderNumber = 0;
+        /// <summary>
+        /// A private backing field of items in an order
+        /// </summary>
         private List<IOrderItem> items = new List<IOrderItem>();
+        /// <summary>
+        /// A property that returns the items in an order
+        /// </summary>
         public IEnumerable<IOrderItem> Items => items.ToArray();
+        /// <summary>
+        /// Returns the subtotal of an order
+        /// </summary>
         public double Subtotal
         {
             get
@@ -21,22 +33,32 @@ namespace CowboyCafe.Data
                 return total;
             }
         }
+        /// <summary>
+        /// Updates the order number for current order
+        /// </summary>
         public uint OrderNumber { get; } = lastOrderNumber++;
+        /// <summary>
+        /// Handles property changes
+        /// </summary>
         public event PropertyChangedEventHandler PropertyChanged;
-        
+        /// <summary>
+        /// Adds an item to an order
+        /// </summary>
+        /// <param name="item"></param>
         public void Add(IOrderItem item)
         {
             items?.Add(item);
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Items"));
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Price"));
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Subtotal"));
         }
-
+        /// <summary>
+        /// Removes an item from an order
+        /// </summary>
+        /// <param name="item"></param>
         public void Remove(IOrderItem item)
         {
             items?.Remove(item);
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Items"));
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Price"));
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Subtotal"));
         }
 
--- PointOfSale/OrderControl.xaml.cs
+++ PointOfSale/OrderControl.xaml.cs
@@ -26,11 +26,21 @@ namespace PointOfSale
             this.DataContext = data;
         }
 
+        /// <summary>
+        /// Handles the cancel order button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void CancelOrder_Click(object sender, RoutedEventArgs e)
         {
             this.DataContext = new Order();
         }
 
+        /// <summary>
+        /// Handles the complete order button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void CompleteOrder_Click(object sender, RoutedEventArgs e)
         {
             this.DataContext = new Order();
